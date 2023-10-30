9a6b3ad11cebc34b40be239ad20eb5c01159b7f6
Alessio Santillo <alessiosantillo31199@gmail.com>
2023-10-30 17:29:23 +0100

    creazione dei file per calc, pixel, spotify, alien

--- ultimo/src/routes/progetti/+page.svelte
+++ ultimo/src/routes/progetti/+page.svelte
@@ -1,5 +1,9 @@
 <script>
-  import 
+  import alien from '/src/routes/img/alien.png';
+  import spotify from '/src/routes/img/spotify.png';
+  import pixel from '/src/routes/img/gamepad.png';
+  import calculator from '/src/routes/img/calculator.png';
+  import ita from '/src/routes/textITA.json';
 </script>
 
 
@@ -13,7 +17,7 @@
 
 <div>
   <div class="bg-black flex-auto relative top-5 w-auto h-24 items-center border-b-2 white flex rounded-3xl ">
-  <h1  class="text-white text-4xl font-bold flex relative left-2 justify-center cursor-default">HomeCode<h1 class="animate-pulse text-white">|</h1></h1>
+  <h1  class="text-white text-4xl font-bold flex relative left-[50px] justify-center cursor-default">HomeCode<h1 class="animate-pulse text-white">|</h1></h1>
   </div>
 
   <div class="bg-zinc-800 h-10 flex-auto flex flex-row items-center justify-between relative top-12  rounded-lg md:w-[60%] md:left-[35%] md:-top-12 "> 
@@ -24,8 +28,40 @@
 </div>
 
 
-<div class="bg-black w-[100%] md:h-[280%] h-[5300px] relative top-36 rounded-3xl flex justify-center">
-    <div class="grid lg:grid-cols-3 lg:grid-rows-3 w-[100%] gap-10 sm:grid-cols-1 md:grid-cols-2">
+<div class="bg-black w-[100%] md:h-[170%] lg:h-[280%] h-[3300px] relative top-36 rounded-3xl flex justify-center">
+    <div class="grid lg:grid-cols-1 lg:grid-rows-4 w-[100%] lg:w-[80%] gap-10 sm:grid-cols-1 md:grid-cols-2">
+       
+      <div class="bg-neutral-900 w-[90%] md:h-[90%] lg:h-[70%] h-[90%] flex justify-center relative top-10 left-[5%] rounded-3xl hover:transition hover:ease-in-out hover:duration-1000 hover:border-solid hover:border-teal-500 hover:border-[5px]">
+        <img src={alien} alt="html" class="w-[60%] md:w-[50%] lg:w-[30%] lg:left-[5%] absolute top-8" />
+        <h1 class="bg-teal-500 flex flex-row w-[100%] h-[10%] absolute top-[36%] font-extrabold text-5xl justify-center items-center lg:top-[5%] lg:w-[40%] lg:right-[13%] lg:rounded-xl ">X-FLIES</h1>
+        <p class="absolute top-[50%] w-[90%] text-justify lg:w-[40%] lg:text-xl lg:right-[13%] lg:top-[20%]">{ita.progetti.alien}</p>
+        <a href="/xflies" class="absolute bottom-8 bg-neutral-950 rounded-3xl w-[80%] text-center p-3 font-semibold text-teal-500 lg:bottom-[5%] lg:w-[40%] lg:right-[13%] lg:h-[10%] text-xl hover:rounded-lg hover:bg-black duration-700 ">Prova X-Flies</a>
+        </div>
+
+        <div class="bg-neutral-900 w-[90%] md:h-[90%] lg:h-[70%] h-[90%] flex justify-center relative top-10 left-[5%] rounded-3xl hover:transition hover:ease-in-out hover:duration-1000 hover:border-solid hover:border-green-500 hover:border-[5px]">
+          <img src={spotify} alt="html" class="w-[60%] md:w-[50%] lg:w-[30%] lg:left-[5%] absolute top-8" />
+          <h1 class="bg-green-500 flex flex-row w-[100%] h-[10%] absolute top-[36%] font-extrabold text-5xl justify-center items-center lg:top-[5%] lg:w-[40%] lg:right-[13%] lg:rounded-xl ">SPOTIFY</h1>
+          <p class="absolute top-[50%] w-[90%] text-justify lg:w-[40%] lg:text-xl lg:right-[13%] lg:top-[20%]">{ita.progetti.spotify}</p>
+          <a href="/spotify" class="absolute bottom-8 bg-neutral-950 rounded-3xl w-[80%] text-center p-3 font-semibold text-teal-500 lg:bottom-[5%] lg:w-[40%] lg:right-[13%] lg:h-[10%] text-xl hover:rounded-lg hover:bg-black duration-700 ">Prova Spotify</a>
+          </div>
+
+          <div class="bg-neutral-900 w-[90%] md:h-[90%] lg:h-[70%] h-[90%] flex justify-center relative top-10 left-[5%] rounded-3xl hover:transition hover:ease-in-out hover:duration-1000 hover:border-solid hover:border-violet-900 hover:border-[5px]">
+            <img src={pixel} alt="html" class="w-[60%] md:w-[50%] lg:w-[30%] lg:left-[5%] absolute top-8" />
+            <h1 class="bg-violet-900 flex flex-row w-[100%] h-[10%] absolute top-[36%] font-extrabold text-5xl justify-center items-center lg:top-[5%] lg:w-[40%] lg:right-[13%] lg:rounded-xl lg:text-[40px] ">PIXEL NIGHTMARE</h1>
+            <p class="absolute top-[50%] w-[90%] text-justify lg:w-[40%] lg:text-xl lg:right-[13%] lg:top-[20%]">{ita.progetti.pixel}</p>
+            <a href="/pixelnightmare" class="absolute bottom-8 bg-neutral-950 rounded-3xl w-[80%] text-center p-3 font-semibold text-teal-500 lg:bottom-[5%] lg:w-[40%] lg:right-[13%] lg:h-[10%] text-xl hover:rounded-lg hover:bg-black duration-700 ">Prova Pixel Nightmare</a>
+            </div>
+
+            <div class="bg-neutral-900 w-[90%] md:h-[90%] lg:h-[70%] h-[90%] flex justify-center relative top-10 left-[5%] rounded-3xl hover:transition hover:ease-in-out hover:duration-1000 hover:border-solid hover:border-red-700 hover:border-[5px]">
+              <img src={calculator} alt="html" class="w-[60%] md:w-[50%] lg:w-[30%] lg:left-[5%] absolute top-8" />
+              <h1 class="bg-red-700 flex flex-row w-[100%] h-[10%] absolute top-[36%] font-extrabold text-5xl justify-center items-center lg:top-[5%] lg:w-[40%] lg:right-[13%] lg:rounded-xl ">CALCULATOR</h1>
+              <p class="absolute top-[50%] w-[90%] text-justify lg:w-[40%] lg:text-[18px] lg:right-[13%] lg:top-[20%]">{ita.progetti.calculator}</p>
+              <a href="/calcolatrice" class="absolute bottom-8 bg-neutral-950 rounded-3xl w-[80%] text-center p-3 font-semibold text-teal-500 lg:bottom-[5%] lg:w-[40%] lg:right-[13%] lg:h-[10%] text-xl hover:rounded-lg hover:bg-black duration-700 ">Prova Calculator</a>
+              </div>
+  
+        
+              
+  
 
     </div>  
 </div>
--- ultimo/src/routes/skills/+page.svelte
+++ ultimo/src/routes/skills/+page.svelte
@@ -19,7 +19,7 @@
 
 <div>
   <div class="bg-black flex-auto relative top-5 w-auto h-24 items-center border-b-2 white flex rounded-3xl ">
-  <h1  class="text-white text-4xl font-bold flex relative left-2 justify-center cursor-default">HomeCode<h1 class="animate-pulse text-white">|</h1></h1>
+  <h1  class="text-white text-4xl font-bold flex relative left-[50px] justify-center cursor-default">HomeCode<h1 class="animate-pulse text-white">|</h1></h1>
   </div>
 
   <div class="bg-zinc-800 h-10 flex-auto flex flex-row items-center justify-between relative top-12  rounded-lg md:w-[60%] md:left-[35%] md:-top-12 "> 
