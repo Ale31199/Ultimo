7bea492e0d148c2198500d754d29f9eaad11b8ef
Alessio Santillo <alessiosantillo31199@gmail.com>
2023-10-30 21:03:19 +0100

    bozza calcolatrice da rifare

--- ultimo/src/routes/calcolatrice/+page.svelte
+++ ultimo/src/routes/calcolatrice/+page.svelte
@@ -1,4 +1,49 @@
+<script>
+  import gameboy from '/src/routes/calcolatrice/gameboy.gif';
+  import devil from '/src/routes/calcolatrice/littledevil.gif';
+  import sticky from '/src/routes/calcolatrice/sticky.gif';
 
+  let result = '';
+  let count = '';
+  const uno = '1';
+  const due = '2';
+  const tre = '3';
+  const quattro = '4';
+  const cinque = '5';
+  const sei = '6';
+  const sette= '7';
+  const otto = '8';
+  const nove= '9';
+  const zero = '0';
+  const piu = '+';
+  const meno = '-';
+  const diviso= '/';
+  const per = '*';
+  const uguale = '=';
+  const punto = '.'
+
+
+  const calc=(input)=>{
+    if (input === '='){
+      result = eval(result);
+    }else if (input === 'Reset'){
+      result = '';
+    }else{
+      result += input
+    }
+  }
+
+  const maxlimit = 9 
+
+  const limit=(event)=>{
+    const text = event.target.value;
+    if (text.length > maxlimit){
+      
+    }
+  }
+
+
+</script>
 
 <div>
   <div class="bg-black flex-auto relative top-5 w-auto h-24 items-center border-b-2 white flex rounded-3xl ">
@@ -12,4 +57,25 @@
   </div>
 </div>
 
-<h1>calcolatrice</h1>
+<div class="w-[90%] h-[90%] absolute top-[27%] left-[5%] rounded-3xl bg-black flex justify-center border-solid border-2 border-gray-700 shadow-2xl bg-gradient-to-tl from-black to-gray-950">
+  <div class="w-[90%] h-[15%] absolute top-[20%] bg-neutral-900 rounded-2xl border-solid border-2 border-gray-700 text-teal-700 items-start flex justify-center text-4xl">{result}</div>
+  <div class="grid grid-cols-4 grid-rows-5 absolute w-[100%] h-[100%] left-[2.5%] top-[5%]">
+    <div on:click="{()=>calc('1')}" class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[160%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">1</div>
+    <div class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[160%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">2</div>
+    <div class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[160%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">3</div>
+    <div on:click="{()=>calc('+')}" class="bg-teal-900 w-[60%] h-[40%] rounded-xl flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[160%] left-[10%] hover:bg-orange-700 hover:rounded-2xl hover:duration-700 hover:ease-in-out hover:cursor-pointer">/</div>
+    <div class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[120%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">4</div>
+    <div class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[120%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">5</div>
+    <div class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[120%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">6</div>
+    <div class="bg-teal-900 w-[60%] h-[40%] rounded-xl flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[120%] left-[10%] hover:bg-orange-700 hover:rounded-2xl hover:duration-700 hover:ease-in-out hover:cursor-pointer">*</div>
+    <div class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[80%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">7</div>
+    <div class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[80%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">8</div>
+    <div class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[80%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">9</div>
+    <div class="bg-teal-900 w-[60%] h-[40%] rounded-xl flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[80%] left-[10%]  hover:bg-orange-700 hover:rounded-2xl hover:duration-700 hover:ease-in-out hover:cursor-pointer">-</div>
+    <div class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[40%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">0</div>
+    <div class="bg-neutral-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[40%] left-[10%]  hover:text-teal-400 hover:bg-neutral-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">.</div>
+    <div on:click="{()=>calc('=')}" class="bg-green-900 w-[60%] h-[40%] rounded-full flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[40%] left-[10%]    hover:bg-green-700 hover:rounded-lg hover:duration-700 hover:ease-in-out hover:cursor-pointer">=</div>
+    <div class="bg-teal-900 w-[60%] h-[40%] rounded-xl flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[40%] left-[10%]  hover:bg-orange-700 hover:rounded-2xl hover:duration-700 hover:ease-in-out hover:cursor-pointer">+</div>
+    <div on:click="{()=>calc('Reset')}" class="bg-red-900 w-[130%] h-[40%] rounded-xl flex justify-center text-center items-center text-white text-4xl font-semibold relative top-[10%] left-[10%]  hover:bg-red-700 hover:rounded-full hover:duration-700 hover:ease-in-out hover:cursor-pointer">Reset</div>
+  </div>
+</div>
