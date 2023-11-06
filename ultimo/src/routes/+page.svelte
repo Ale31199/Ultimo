e38095d38c9f65d43093a0ae7b8d2043af46b3af
Alessio Santillo <alessiosantillo31199@gmail.com>
2023-11-06 14:41:17 +0100

    home page fix responsive, ora ok!

--- ultimo/src/routes/+page.svelte
+++ ultimo/src/routes/+page.svelte
@@ -74,14 +74,17 @@
     <img alt="io" src={io} class="w-[25%] top-[1.5%] left-[5%] sm:w-[25%] sm:left-[5%] sm:top-[1%] md:left-[5%] md:top-[1%] rounded-full md:w-[25%] lg:w-[20%] 2xl:w-[15%] border-2 border-teal-500 absolute"/>
     <h1 class="text-teal-500 font-bold text-4xl sm:text-6xl sm:top-[1%] lg:top-[2.5%] md:text-7xl absolute top-7 md:top-8 left-[35%] lg:left-[27%] ">{ita.testo.titolo}</h1>
 
-    <div class="w-[80%] sm:w-[60%] md:w-[80%] h-[240px] lg:h-[100px] text-sm md:text-lg lg:text-base absolute top-[10.5%] sm:top-[14%] md:top-[18%] lg:top-[20%] md:left-[10%] rounded-xl grid grid-cols-1 grid-rows-4 md:grid-cols-1 md:grid-rows-4 md:gap-x-[1%] lg:grid-cols-2 lg:grid-rows-2 lg:gap-x-[1%] lg:gap-y-[0%] items-center ">
-    <p class="bg-neutral-800 inline-flex rounded-lg p-1 cursor-default justify-center">Email: alessiosantillo31199@gmail.com</p>
-    <p class="bg-neutral-800 inline-flex rounded-lg p-1 cursor-default justify-center">Telefono: 3898938262</p>
-    <a href="https://github.com/Ale31199" class="bg-neutral-800 inline-flex rounded-lg p-1 cursor-pointer underline text-teal-500 justify-center">Github: Ale31199 </a>
-    <a href="https://www.linkedin.com/in/alessio-santillo-422186275/" class="bg-neutral-800 inline-flex text-teal-500 rounded-lg p-1 cursor-pointer underline justify-center ">LinkedIn: Alessio Santillo</a>
+    
+    <div class="w-[100%] lg:w-[70%] xl:w-[50%] h-[240px] lg:h-[100px] text-sm md:text-lg lg:text-base absolute top-[10.5%] sm:top-[14%] md:top-[18%] lg:top-[20%]  rounded-xl grid grid-cols-1 grid-rows-4 md:grid-cols-1 md:grid-rows-4 md:gap-x-[1%] lg:grid-cols-2 lg:grid-rows-2 lg:gap-x-[1%] lg:gap-y-[0%] justify-items-center items-center ">
+    <p class="bg-neutral-800 inline-flex w-[310px] sm:w-[500px] md:w-[600px] lg:w-[300px] rounded-lg p-1 cursor-default justify-center">Email: alessiosantillo31199@gmail.com</p>
+    <p class="bg-neutral-800 inline-flex w-[310px] sm:w-[500px] md:w-[600px] lg:w-[300px] rounded-lg p-1 cursor-default justify-center">Telefono: 3898938262</p>
+    <a href="https://github.com/Ale31199" class="bg-neutral-800 w-[310px] sm:w-[500px] md:w-[600px] lg:w-[300px] inline-flex rounded-lg p-1 cursor-pointer underline text-teal-500 justify-center">Github: Ale31199 </a>
+    <a href="https://www.linkedin.com/in/alessio-santillo-422186275/" class="bg-neutral-800 w-[310px] sm:w-[500px] md:w-[600px] lg:w-[300px] inline-flex text-teal-500 rounded-lg p-1 cursor-pointer underline justify-center ">LinkedIn: Alessio Santillo</a>
     </div>
+  
 
-    <div class="w-[95%] sm:w-[80%] h-[1400px] sm:h-[1200px] md:h-[1100px] lg:h-[1000px] xl:h-[1100px] absolute bottom-[0%] sm:bottom-[0%] bg-gradient-to-bl from-neutral-800 to-neutral-950 border-2 border-neutral-800 grid grid-cols-1 grid-rows-3 justify-items-center text-justify rounded-3xl">
+
+    <div class="w-[310px] sm:w-[500px] md:w-[600px] lg:w-[750px] 2xl:w-[1000px] h-[1400px] sm:h-[1200px] md:h-[1100px] lg:h-[1000px] xl:h-[1100px] absolute bottom-[0%] sm:bottom-[0%] bg-gradient-to-bl from-neutral-800 to-neutral-950 border-2 border-neutral-800 grid grid-cols-1 grid-rows-3 justify-items-center text-justify rounded-3xl">
       
       <div class="flex flex-col items-center">
     <p class="text-justify ml-4 mr-4 text-2xl relative top-[3%] sm:top-[5%] md:top-[3%] lg:top-[7%] md:text-3xl lg:text-4xl  font-extrabold" >Chi sono?</p>
