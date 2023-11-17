6db30c9ff0a78f94d33bd74cb06eb9d482dca871
Alessio Santillo <alessiosantillo31199@gmail.com>
2023-11-17 14:53:03 +0100

    pagina home tradotta

--- ultimo/src/routes/+page.svelte
+++ ultimo/src/routes/+page.svelte
@@ -2,7 +2,33 @@
   import io from '/src/routes/img/io.jpg';
   // @ts-ignore
   import ita from '/src/routes/textITA.json';
+  // @ts-ignore
+  import eng from '/src/routes/textENG.json';
+  
+  import { lingua } from './lingua.js';
 
+  import { onMount } from 'svelte'
+
+
+  let testi;
+
+  onMount(() => {
+    testi = $lingua === ita ? ita : eng;
+  });
+
+
+  $: testi = $lingua === ita ? ita : eng;
+
+  const TraduciPagina=(linguatraduci)=>{
+    switch(linguatraduci){
+      case 'ita':
+        lingua.set(ita)
+        break;
+      case 'eng':
+        lingua.set(eng)
+      break;
+    }
+  }
  
 
  let home= true
@@ -67,22 +93,25 @@
     <div class="bg-gradient-to-tr from-black to-neutral-900 border-2 border-neutral-800 h-10 flex-auto flex flex-row items-center justify-between relative top-12 rounded-xl md:w-[50%] md:left-[45%] md:-top-12 "> 
     <a href="/" class=" font-bold ml-6 cursor-pointer hover:text-teal-500 hover:border-b-2 hover:border-teal-500 {home ? "border-b-2 rounded-sm border-teal-500 text-teal-500":"text-white"}" on:click="{()=>selezionaPag('home')}">Home</a>
     <a href="/skills" class=" text-white font-bold cursor-pointer hover:text-teal-500 hover:border-b-2 hover:border-teal-500 {skills ? "border-b-2 border-teal-500":""}" on:click="{()=>selezionaPag('skills')}">Skills</a>
-    <a href="/progetti" class=" text-white font-bold mr-6 cursor-pointer hover:text-teal-500 hover:border-b-2 hover:border-teal-500 {progetti ? "border-b-2 border-teal-500":""}" on:click="{()=>selezionaPag('progetti')}">Progetti</a>
+    <a href="/progetti" class=" text-white font-bold mr-6 cursor-pointer hover:text-teal-500 hover:border-b-2 hover:border-teal-500 {progetti ? "border-b-2 border-teal-500":""}" on:click="{()=>selezionaPag('progetti')}">{$lingua.testo.progetti}</a>
     </div>
 </div>
 
-
+<select on:change="{(event)=>TraduciPagina(event.target.value)}" class="w-[100px] absolute top-[30%] sm:top-[20%] bg-gradient-to-tr p-2 from-white to-neutral-300 border-2 border-neutral-300 rounded-2xl invert">
+<option value="ita">Italiano</option>
+<option value="eng">English</option>
+</select>
 
 
 <div class="flex justify-center w-[100%] sm:h-[1900px] h-[2200px] md:h-[1900px] lg:h-[1650px] xl:h-[1750px]">
 <div class="bg-black w-[100%] sm:h-[1700px] h-[2000px] md:h-[1700px] lg:h-[1450px] xl:h-[1550px] rounded-3xl relative top-36 border-teal-500 border-b-4 border-r-4 flex justify-center ">
     <img alt="io" src={io} class="w-[25%] top-[1.5%] left-[5%] sm:w-[25%] sm:left-[5%] sm:top-[1%] md:left-[5%] md:top-[1%] rounded-full md:w-[25%] lg:w-[20%] 2xl:w-[15%] border-2 border-teal-500 absolute"/>
-    <h1 class="text-teal-500 font-bold text-4xl sm:text-6xl sm:top-[1%] lg:top-[2.5%] md:text-7xl absolute top-7 md:top-8 left-[35%] lg:left-[27%] ">{ita.testo.titolo}</h1>
+    <h1 class="text-teal-500 font-bold text-4xl sm:text-6xl sm:top-[1%] lg:top-[2.5%] md:text-7xl absolute top-7 md:top-8 left-[35%] lg:left-[27%] ">{$lingua.testo.titolo}</h1>
 
     
     <div class="w-[100%] lg:w-[80%] xl:w-[60%] h-[240px] lg:h-[100px] text-sm md:text-lg lg:text-base absolute top-[10.5%] sm:top-[14%] md:top-[18%] lg:top-[20%]  rounded-xl grid grid-cols-1 grid-rows-4 md:grid-cols-1 md:grid-rows-4 md:gap-x-[1%] lg:grid-cols-2 lg:grid-rows-2 lg:gap-x-[1%] lg:gap-y-[0%] justify-items-center items-center ">
     <p class="bg-neutral-800 inline-flex w-[310px] sm:w-[500px] md:w-[600px] lg:w-[300px] rounded-lg p-1 cursor-default justify-center">Email: alessiosantillo31199@gmail.com</p>
-    <p class="bg-neutral-800 inline-flex w-[310px] sm:w-[500px] md:w-[600px] lg:w-[300px] rounded-lg p-1 cursor-default justify-center">Telefono: 3898938262</p>
+    <p class="bg-neutral-800 inline-flex w-[310px] sm:w-[500px] md:w-[600px] lg:w-[300px] rounded-lg p-1 cursor-default justify-center">{$lingua.testo.tel}: 3898938262</p>
     <a href="https://github.com/Ale31199" class="bg-neutral-800 w-[310px] sm:w-[500px] md:w-[600px] lg:w-[300px] inline-flex rounded-lg p-1 cursor-pointer underline text-teal-500 justify-center">Github: Ale31199 </a>
     <a href="https://www.linkedin.com/in/alessio-santillo-422186275/" class="bg-neutral-800 w-[310px] sm:w-[500px] md:w-[600px] lg:w-[300px] inline-flex text-teal-500 rounded-lg p-1 cursor-pointer underline justify-center ">LinkedIn: Alessio Santillo</a>
     </div>
@@ -92,18 +121,18 @@
     <div class="w-[310px] sm:w-[500px] md:w-[600px] lg:w-[750px] 2xl:w-[1000px] h-[1500px] sm:h-[1200px] md:h-[1100px] lg:h-[1000px] xl:h-[1100px] absolute bottom-[0%] sm:bottom-[0%] bg-gradient-to-bl from-neutral-800 to-neutral-950 border-2 border-neutral-800 grid grid-cols-1 grid-rows-3 justify-items-center text-justify rounded-3xl">
       
       <div class="flex flex-col items-center">
-    <p class="text-justify ml-4 mr-4 text-2xl relative top-[3%] sm:top-[5%] md:top-[3%] lg:top-[7%] md:text-3xl lg:text-4xl  font-extrabold" >Chi sono?</p>
-    <p class="text-justify ml-4 mr-4 absolute top-[4%] sm:top-[5%] lg:top-[7%] font-thin text-[12px] sm:text-[13px] lg:text-[15px] xl:text-[15px] 2xl:text-[17px] md:text-[14px] w-[90%] sm:w-[80%] " >{ita.testo.testo2}</p>
+    <p class="text-justify ml-4 mr-4 text-2xl relative top-[3%] sm:top-[5%] md:top-[3%] lg:top-[7%] md:text-3xl lg:text-4xl  font-extrabold" >{$lingua.testo.chi}</p>
+    <p class="text-justify ml-4 mr-4 absolute top-[4%] sm:top-[5%] lg:top-[7%] font-thin text-[12px] sm:text-[13px] lg:text-[15px] xl:text-[15px] 2xl:text-[17px] md:text-[14px] w-[90%] sm:w-[80%] " >{$lingua.testo.testo2}</p>
       </div>
 
       <div class="flex flex-col items-center">
-    <p class="text-justify ml-4 mr-4 text-2xl absolute top-[50%] sm:top-[48%] md:top-[51%] lg:top-[50%] xl:top-[49%] md:text-3xl lg:text-4xl font-extrabold" >Il Mio Percorso  </p>
-    <p class="text-justify ml-4 mr-4 absolute top-[53%] sm:top-[52%] md:top-[55%] lg:top-[55%] xl:top-[54%] font-thin text-[12px] sm:text-[13px] lg:text-[14px] xl:text-[15px] 2xl:text-[17px] md:text-[13px] w-[90%] sm:w-[80%]" >{ita.testo.testo3}</p>
+    <p class="text-justify ml-4 mr-4 text-2xl absolute top-[50%] sm:top-[48%] md:top-[51%] lg:top-[50%] xl:top-[49%] md:text-3xl lg:text-4xl font-extrabold" >{$lingua.testo.percorso}</p>
+    <p class="text-justify ml-4 mr-4 absolute top-[53%] sm:top-[52%] md:top-[55%] lg:top-[55%] xl:top-[54%] font-thin text-[12px] sm:text-[13px] lg:text-[14px] xl:text-[15px] 2xl:text-[17px] md:text-[13px] w-[90%] sm:w-[80%]" >{$lingua.testo.testo3}</p>
       </div>
 
       <div class="flex flex-col items-center">
-    <p class="text-justify ml-4 mr-4 text-2xl absolute top-[84%] sm:top-[82%] lg:top-[81%] md:text-3xl lg:text-4xl  font-extrabold" >Il Futuro  </p>
-    <p class="text-justify ml-4 mr-4 absolute top-[87%] sm:top-[86%] font-thin text-[12px] sm:text-[13px] lg:text-[15px] md:text-[14px] xl:text-[15px] 2xl:text-[17px] w-[90%] sm:w-[80%]" >{ita.testo.testo4}</p>
+    <p class="text-justify ml-4 mr-4 text-2xl absolute top-[84%] sm:top-[82%] lg:top-[81%] md:text-3xl lg:text-4xl  font-extrabold" >{$lingua.testo.futuro}</p>
+    <p class="text-justify ml-4 mr-4 absolute top-[87%] sm:top-[86%] font-thin text-[12px] sm:text-[13px] lg:text-[15px] md:text-[14px] xl:text-[15px] 2xl:text-[17px] w-[90%] sm:w-[80%]" >{$lingua.testo.testo4}</p>
     </div>
 
   </div>
